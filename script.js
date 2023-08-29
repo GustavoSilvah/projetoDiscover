238d73a21be52ff71e9568d68346959720672af4
Gustavo Silva <gustavosilva.gs2001@hotmail.com>
2023-08-28 21:08:22 -0300

    profile fix bug

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/kaguya-icon.png")
+    img.setAttribute("src", "https://i.imgur.com/BhIlk19.png")
   } else {
     img.setAttribute("src", "./assets/kaguya-icon-dark.png")
   }
